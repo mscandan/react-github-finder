38a1083bc38e531d7fd5653c21961fa07572e467
Mehmet Selcuk Candan <mehmetselcukcandan@icloud.com>
2021-01-09 23:39:26 +0300

    file structure cleaning

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import "../styles/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p>GitHub profile finder app with reactjs</p>
+    </div>
+  );
+}
+
+export default App;
