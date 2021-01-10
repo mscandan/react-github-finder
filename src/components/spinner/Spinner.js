9bd59a27a6a65056610c8aefb9b0dc4a6f597b25
Mehmet Selcuk Candan <mehmetselcukcandan@icloud.com>
2021-01-10 22:42:10 +0300

    spinner added

---
+++ src/components/spinner/Spinner.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import spinner from './spinner.gif';
+import '../../styles/Spinner.css';
+
+const Spinner = () => {
+  return (
+    <Fragment>
+      <img className="spinner" src={spinner} alt="loading" />
+    </Fragment>
+  );
+};
+
+export default Spinner;
