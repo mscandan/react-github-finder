9214b8a7a38f4b0f00859a971b0fba8cf72791b4
Mehmet Selcuk Candan <mehmetselcukcandan@icloud.com>
2021-01-15 22:14:26 +0300

    new env update

--- src/components/spinner/Spinner.js
+++ src/components/spinner/Spinner.js
@@ -1,6 +1,6 @@
-import React, { Fragment } from 'react';
-import spinner from './spinner.gif';
-import '../../styles/Spinner.css';
+import React, { Fragment } from "react";
+import spinner from "./spinner.gif";
+import "../../styles/Spinner.css";
 
 const Spinner = () => {
   return (
