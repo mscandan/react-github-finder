5b17e43f73921ed681d8b76243b3283d93db6d6a
Mehmet Selcuk Candan <mehmetselcukcandan@icloud.com>
2021-01-16 19:05:19 +0300

    added search button functionality

--- src/components/App.js
+++ src/components/App.js
@@ -6,26 +6,36 @@ import axios from 'axios';
 import Search from './users/Search';
 class App extends Component {
   state = {
-    api_url:
-      'https://api.github.com/users?client_id=' +
-      process.env.REACT_APP_GITHUB_CLIENT_ID +
-      '&client_secret=' +
-      process.env.REACT_APP_GITHUB_CLIENT_SECRET,
+    client_id: process.env.REACT_APP_GITHUB_CLIENT_ID,
+    client_secret: process.env.REACT_APP_GITHUB_CLIENT_SECRET,
     users: [],
     loading: false,
   };
 
-  async componentDidMount() {
+  // display some users
+  async getData(searchUrl) {
     this.setState({ loading: true });
-    const res = await axios.get(this.state.api_url);
-    this.setState({ users: res.data, loading: false });
+    const res = await axios.get(searchUrl);
+    this.setState({ users: res.data.items, loading: false });
   }
+
+  // Search users
+  searchUsers = (text) => {
+    const query =
+      'https://api.github.com/search/users?q=' +
+      text +
+      '&client_id=' +
+      this.state.client_id +
+      '&client_secret=' +
+      this.state.client_secret;
+    this.getData(query);
+  };
 
   render() {
     return (
       <div className='App'>
         <Navbar />
-        <Search />
+        <Search searchUsers={this.searchUsers} />
         <div className='container'>
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 import '../../styles/Search.css';
+import PropTypes from 'prop-types';
 
 export class Search extends Component {
   state = {
     text: '',
   };
+
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+  };
+
   onChange = (e) => this.setState({ [e.target.name]: e.target.value });
   onSubmit = (e) => {
     e.preventDefault();
-    console.log(this.state.text);
+    this.props.searchUsers(this.state.text);
+    this.setState({ text: '' });
   };
   render() {
     return (
